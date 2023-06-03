fd9a38b936a9c4b6d6c0c165811103ee78bf8f96
kennedy-anon <85305895+kennedy-anon@users.noreply.github.com>
2023-06-03 11:32:19 +0300

    retaining form old values after submit error

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -18,7 +18,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="company"
+                name="company" value="{{old('company')}}"
             />
 
             @error('company')
@@ -34,7 +34,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="title"
-                placeholder="Example: Senior Laravel Developer"
+                placeholder="Example: Senior Laravel Developer" value="{{old('title')}}"
             />
 
             @error('title')
@@ -52,7 +52,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="location"
-                placeholder="Example: Remote, Boston MA, etc"
+                placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}"
             />
 
             @error('location')
@@ -67,7 +67,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="email"
+                name="email" value="{{old('email')}}"
             />
 
             @error('email')
@@ -85,7 +85,7 @@
             <input
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="website"
+                name="website" value="{{old('website')}}"
             />
 
             @error('website')
@@ -101,7 +101,7 @@
                 type="text"
                 class="border border-gray-200 rounded p-2 w-full"
                 name="tags"
-                placeholder="Example: Laravel, Backend, Postgres, etc"
+                placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}"
             />
 
             @error('tags')
@@ -132,7 +132,7 @@
                 name="description"
                 rows="10"
                 placeholder="Include tasks, requirements, salary, etc"
-            ></textarea>
+            >{{old('description')}}</textarea>
 
             @error('description')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
